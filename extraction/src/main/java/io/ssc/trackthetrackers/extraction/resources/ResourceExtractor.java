8fb9bb3a8bfe90cc7eb3fd9e2ac5c14024b6d63f
FelixNeutatz <neutatz@googlemail.com>
2015-01-19 17:26:15 +0100

    clean up

--- extraction/src/main/java/io/ssc/trackthetrackers/extraction/resources/ResourceExtractor.java
+++ extraction/src/main/java/io/ssc/trackthetrackers/extraction/resources/ResourceExtractor.java
@@ -1,6 +1,26 @@
+/**
+ * Track the trackers
+ * Copyright (C) 2014  Sebastian Schelter
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 package io.ssc.trackthetrackers.extraction.resources;
 
 import com.google.common.collect.Sets;
+
 import com.google.javascript.jscomp.parsing.Config;
 import com.google.javascript.jscomp.parsing.Config.LanguageMode;
 import com.google.javascript.jscomp.parsing.ParserRunner;
@@ -8,15 +28,19 @@ import com.google.javascript.rhino.ErrorReporter;
 import com.google.javascript.rhino.Node;
 import com.google.javascript.rhino.jstype.SimpleSourceFile;
 import com.google.javascript.rhino.jstype.StaticSourceFile;
+
 import org.apache.commons.validator.routines.DomainValidator;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
